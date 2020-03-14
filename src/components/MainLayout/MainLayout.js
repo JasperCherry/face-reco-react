cb6b77f522ec9d7448f1897ad12b9bc4a9d6ffe3
JasperCherry <kacper.wisnicki@gmail.com>
2020-03-14 23:07:46 +0000

    Added styled components

---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Title,
+} from './MainLayout.styled';
+
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Title>MainLayout Comp</Title>
+    </div>
+  );
+}
+
+
+export default MainLayout;
