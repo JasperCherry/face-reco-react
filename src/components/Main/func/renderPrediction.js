9bbf5535338b1a95375154ab62595ce0fc4c5cd4
JasperCherry <kacper.wisnicki@gmail.com>
2020-03-15 12:47:29 +0000

    Added named landmarks

--- src/components/Main/func/renderPrediction.js
+++ src/components/Main/func/renderPrediction.js
@@ -19,27 +19,38 @@ export const renderPrediction = async ({ video, canvas, ctx, model }) => {
 
       if (annotateBoxes) {
         const landmarks = predictions[i].landmarks;
+        const landmarksNamed = {};
 
         for (let j = 0; j < landmarks.length; j++) {
           if (j === 0) {
+            landmarksNamed.rightEye = landmarks[j];
             ctx.fillStyle = "red";
           } else if (j === 1) {
+            landmarksNamed.leftEye = landmarks[j];
             ctx.fillStyle = "blue";
           } else if (j === 2) {
+            landmarksNamed.nose = landmarks[j];
             ctx.fillStyle = "orange";
           } else if (j === 3) {
+            landmarksNamed.mouth = landmarks[j];
             ctx.fillStyle = "pink";
           } else if (j === 4) {
+            landmarksNamed.rightEar = landmarks[j];
             ctx.fillStyle = "yellow";
           } else if (j === 5) {
+            landmarksNamed.leftEar = landmarks[j];
             ctx.fillStyle = "white";
           }
 
           const x = landmarks[j][0];
           const y = landmarks[j][1];
           ctx.fillRect(x, y, 5, 5);
+
+          predictions[i].landmarksNamed = landmarksNamed;
         }
       }
+
+      console.log({ predictions });
     }
   }
 
